7c6d5fefb595d5e2d96da9b73468dd0ba888adb4
antoshre <robert@antosh.net>
2020-10-13 01:11:41 -0700

    Remove Boost::format dependency in favor of fmtlib.

--- lib/include/chip8/disasm/Instructions.h
+++ lib/include/chip8/disasm/Instructions.h
@@ -44,7 +44,18 @@ namespace chip8::disasm {
 
 					//casts are needed because std::uint8_t is typedef'ed as unsigned char or similar
 					//boost format would default-format it as an (unprintable) char before applying formatting
-					return fmt::format(this->underlying().cequiv, nnn(), x(), y(), kk(), n());
+					auto nnn_s = fmt::format("{:#06x}", nnn());
+					auto x_s = fmt::format("{:02x}", x());
+					auto y_s = fmt::format("{:02x}", y());
+					auto kk_s = fmt::format("{:02x}", kk());
+					auto n_s = fmt::format("{:02x}", n());
+					return fmt::format(this->underlying().cequiv,
+					                   fmt::arg("NNN", nnn()),
+					                   fmt::arg("X", x()),
+					                   fmt::arg("Y", y()),
+					                   fmt::arg("NN", kk()),
+					                   fmt::arg("N", n())
+					);
 					/*
 					return boost::str(ignore_extra_args(this->underlying().cequiv)
 					                  % (unsigned int) nnn()
@@ -96,147 +107,147 @@ namespace chip8::disasm {
 		//1NNN 	Jumps to address NNN.
 		struct JUMPI : public detail::inst_base<JUMPI> {
 			constexpr static const char *mnemonic{"JUMPI"};
-			constexpr static const char *cequiv{"goto %1$#05x"};
+			constexpr static const char *cequiv{"goto {NNN:#05x}"};
 		};
 		//2NNN 	Calls subroutine at NNN.
 		struct CALLI : public detail::inst_base<CALLI> {
 			constexpr static const char *mnemonic{"CALLI"};
-			constexpr static const char *cequiv{"call %1$#05x"};
+			constexpr static const char *cequiv{"call {NNN:#05x}"};
 		};
 		//3XNN 	Skips the next instruction if VX equals NN. (Usually the next instruction is a jump to skip a code block)
 		struct SKEI : public detail::inst_base<SKEI> {
 			constexpr static const char *mnemonic{"SKEI"};
-			constexpr static const char *cequiv{"if (V%2$x == %1$#05x)"};
+			constexpr static const char *cequiv{"if (V{X:x} == {NN:#04x})"};
 		};
 		//4XNN 	Skips the next instruction if VX doesn't equal NN. (Usually the next instruction is a jump to skip a code block)
 		struct SKNEI : public detail::inst_base<SKNEI> {
 			constexpr static const char *mnemonic{"SKNEI"};
-			constexpr static const char *cequiv{"if (V%2$x != %1$#05x)"};
+			constexpr static const char *cequiv{"if (V{Y:x} != {NN:#04x})"};
 		};
 		//5XY0 	Skips the next instruction if VX equals VY. (Usually the next instruction is a jump to skip a code block)
 		struct SKER : public detail::inst_base<SKER> {
 			constexpr static const char *mnemonic{"SKER"};
-			constexpr static const char *cequiv{"if (V%2$x == V%3$x)"};
+			constexpr static const char *cequiv{"if (V{X:x} == V{Y:x})"};
 		};
-		//6XNN 	Sets VX to NN.
+		//6XNN 	Sets VX to kk.
 		struct SETI : public detail::inst_base<SETI> {
 			constexpr static const char *mnemonic{"SETI"};
-			constexpr static const char *cequiv{"V%2$x = %5$#03x"};
+			constexpr static const char *cequiv{"V{X:1x} = {NN:#04x}"};
 		};
 		//7XNN 	Adds NN to VX. (Carry flag is not changed)
 		struct ADDI : public detail::inst_base<ADDI> {
 			constexpr static const char *mnemonic{"ADDI"};
-			constexpr static const char *cequiv{"V%2$x += %1$#05x"};
+			constexpr static const char *cequiv{"V{X:x} += {NN:#04x}"};
 		};
 		//8XY0 	Sets VX to the value of VY.
 		struct SETR : public detail::inst_base<SETR> {
 			constexpr static const char *mnemonic{"SETR"};
-			constexpr static const char *cequiv{"V%2$x = V%3$x"};
+			constexpr static const char *cequiv{"V{X:x} = V{Y:x}"};
 		};
 		//8XY1 	Vx=Vx|Vy 	Sets VX to VX or VY. (Bitwise OR operation)
 		struct ORR : public detail::inst_base<ORR> {
 			constexpr static const char *mnemonic{"ORR"};
-			constexpr static const char *cequiv{"V%2$x |= V%3$x"};
+			constexpr static const char *cequiv{"V{X:x} |= V{Y:x}"};
 		};
 		//8XY2 	Vx=Vx&Vy 	Sets VX to VX and VY. (Bitwise AND operation)
 		struct ANDR : public detail::inst_base<ANDR> {
 			constexpr static const char *mnemonic{"ANDR"};
-			constexpr static const char *cequiv{"V%2$x &= V%3$x"};
+			constexpr static const char *cequiv{"V{X:x} &= V{Y:x}"};
 		};
 		//8XY3[a] 	BitOp 	Vx=Vx^Vy 	Sets VX to VX xor VY.
 		struct XORR : public detail::inst_base<XORR> {
 			constexpr static const char *mnemonic{"XORR"};
-			constexpr static const char *cequiv{"V%2$x ^= V%3$x"};
+			constexpr static const char *cequiv{"V{X:x} ^= V{Y:x}"};
 		};
 		//8XY4 	Math 	Vx += Vy 	Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
 		struct ADDR : public detail::inst_base<ADDR> {
 			constexpr static const char *mnemonic{"ADDR"};
-			constexpr static const char *cequiv{"V%2$x += V%3$x"};
+			constexpr static const char *cequiv{"V{X:x} += V{Y:x}"};
 		};
 		//8XY5 	Math 	Vx -= Vy 	VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
 		struct SUBR : public detail::inst_base<SUBR> {
 			constexpr static const char *mnemonic{"SUBR"};
-			constexpr static const char *cequiv{"V%2$x -= V%3$x"};
+			constexpr static const char *cequiv{"V{X:x} -= V{Y:x}"};
 		};
 		//8XY6[a] 	BitOp 	Vx>>=1 	Stores the least significant bit of VX in VF and then shifts VX to the right by 1.[b]
 		struct SHR : public detail::inst_base<SHR> {
 			constexpr static const char *mnemonic{"SHR"};
-			constexpr static const char *cequiv{"V%2$x >>= 1"};
+			constexpr static const char *cequiv{"V{X:x} >>= 1"};
 		};
 		//8XY7[a] 	Math 	Vx=Vy-Vx 	Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
 		struct SUBR_R : public detail::inst_base<SUBR_R> {
 			constexpr static const char *mnemonic{"SUBR_R"};
-			constexpr static const char *cequiv{"V%2$x = V%3$x - V%2$x"};
+			constexpr static const char *cequiv{"V{X:x} = V{Y:x} - V{X:x}"};
 		};
 		//8XYE[a] 	BitOp 	Vx<<=1 	Stores the most significant bit of VX in VF and then shifts VX to the left by 1.[b]
 		struct SHL : public detail::inst_base<SHL> {
 			constexpr static const char *mnemonic{"SHL"};
-			constexpr static const char *cequiv{"V%2$x <<= 1"};
+			constexpr static const char *cequiv{"V{X:x} <<= 1"};
 		};
 		//9XY0 	Cond 	if(Vx!=Vy) 	Skips the next instruction if VX doesn't equal VY. (Usually the next instruction is a jump to skip a code block)
 		struct SKNER : public detail::inst_base<SKNER> {
 			constexpr static const char *mnemonic{"SKNER"};
-			constexpr static const char *cequiv{"if (V%2$x != V%3$x)"};
+			constexpr static const char *cequiv{"if (V{X:x} != V{Y:x})"};
 		};
 		//ANNN 	MEM 	I = NNN 	Sets I to the address NNN.
 		struct ISETI : public detail::inst_base<ISETI> {
 			constexpr static const char *mnemonic{"ISETI"};
-			constexpr static const char *cequiv{"I = %1$#05x"};
+			constexpr static const char *cequiv{"I = {NNN:#05x}"};
 		};
 		//BNNN 	Flow 	PC=V0+NNN 	Jumps to the address NNN plus V0.
 		struct JUMPIND : public detail::inst_base<JUMPIND> {
 			constexpr static const char *mnemonic{"JUMPIND"};
-			constexpr static const char *cequiv{"PC = V%2$x + %1$#05x"};
+			constexpr static const char *cequiv{"PC = V{X:x} + {NNN:#05x}"};
 		};
 		//CXNN 	Rand 	Vx=rand()&NN 	Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
 		struct RAND : public detail::inst_base<RAND> {
 			constexpr static const char *mnemonic{"RAND"};
-			constexpr static const char *cequiv{"V%2$x = rand() & {3:02X}"};
+			constexpr static const char *cequiv{"V{X:x} = rand() & {NN:#04x}"};
 		};
 		//DXYN 	Disp 	draw(Vx,Vy,N) 	Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N+1 pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value doesn’t change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn’t happen
 		struct DRAW : public detail::inst_base<DRAW> {
 			constexpr static const char *mnemonic{"DRAW"};
-			constexpr static const char *cequiv{"draw(V%2$X, V%3$x, %5$#03x)"};
+			constexpr static const char *cequiv{"draw(V{X:x}, V{Y:x}, {N})"};
 		};
 		//EX9E 	KeyOp 	if(key()==Vx) 	Skips the next instruction if the key stored in VX is pressed. (Usually the next instruction is a jump to skip a code block)
 		struct SKEK : public detail::inst_base<SKEK> {
 			constexpr static const char *mnemonic{"SKEK"};
-			constexpr static const char *cequiv{"if (key() == V%2$x)"};
+			constexpr static const char *cequiv{"if (key() == V{X:x})"};
 		};
 		//EXA1 	KeyOp 	if(key()!=Vx) 	Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block)
 		struct SKNEK : public detail::inst_base<SKNEK> {
 			constexpr static const char *mnemonic{"SKNEK"};
-			constexpr static const char *cequiv{"if (key() != V%2$x)"};
+			constexpr static const char *cequiv{"if (key() != V{X:x})"};
 		};
 		//FX07 	Timer 	Vx = get_delay() 	Sets VX to the value of the delay timer.
 		struct GETD : public detail::inst_base<GETD> {
 			constexpr static const char *mnemonic{"GETD"};
-			constexpr static const char *cequiv{"V%2$x = get_delay()"};
+			constexpr static const char *cequiv{"V{X:x} = get_delay()"};
 		};
 		//FX0A 	KeyOp 	Vx = get_key() 	A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event)
 		struct KEY : public detail::inst_base<KEY> {
 			constexpr static const char *mnemonic{"KEY"};
-			constexpr static const char *cequiv{"V%2$x = get_key()"};
+			constexpr static const char *cequiv{"V{X:x} = get_key()"};
 		};
 		//FX15 	Timer 	delay_timer(Vx) 	Sets the delay timer to VX.
 		struct SETD : public detail::inst_base<SETD> {
 			constexpr static const char *mnemonic{"SETD"};
-			constexpr static const char *cequiv{"delay = V%2$x"};
+			constexpr static const char *cequiv{"delay = V{X:x}"};
 		};
 		//FX18 	Sound 	sound_timer(Vx) 	Sets the sound timer to VX.
 		struct SETS : public detail::inst_base<SETS> {
 			constexpr static const char *mnemonic{"SETS"};
-			constexpr static const char *cequiv{"sound = V%2$x"};
+			constexpr static const char *cequiv{"sound = V{X:x}"};
 		};
 		//FX1E 	MEM 	I +=Vx 	Adds VX to I. VF is not affected.[c]
 		struct IADDR : public detail::inst_base<IADDR> {
 			constexpr static const char *mnemonic{"IADDR"};
-			constexpr static const char *cequiv{"I += V%2$x"};
+			constexpr static const char *cequiv{"I += V{X:x}"};
 		};
 		//FX29 	MEM 	I=sprite_addr[Vx] 	Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
 		struct SPRITE : public detail::inst_base<SPRITE> {
 			constexpr static const char *mnemonic{"SPRITE"};
-			constexpr static const char *cequiv{"I = sprite(V%2$x)"};
+			constexpr static const char *cequiv{"I = sprite(V{X:x})"};
 		};
 		//FX33 	BCD 	set_BCD(Vx);
 		//
